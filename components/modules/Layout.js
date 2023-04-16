eac7595e091bea9b8933636a4edd75fac8f8a93d
miss-f-g <fatemeghafari98@gmail.com>
2023-04-16 15:00:13 +0430

    25.fix the close btn issue

--- components/modules/Layout.js
+++ components/modules/Layout.js
@@ -25,15 +25,11 @@ function Layout({ children }) {
   function handleDarkModeToggle() {
     setDarkMode(!darkMode);
   }
-  // height: 100%;
-  // max-height: fit-content;
-  // min-height: 100vh;
 
   return (
-    // <div className="2xl:max-w-screen-xl 2xl:h-fit 2xl:mx-auto p-4 bg-[#EFE9E9] dark:bg-neutral-900 lg:overflow-y-hidden text-neutral-900 dark:text-[#EFE9E9]">
-    <div className="min-h-screen lg:min-h-fit 2xl:max-w-screen-xl lg:h-fit 2xl:w-fit lg:mx-auto p-4 bg-[#EFE9E9] dark:bg-neutral-900 lg:overflow-y-hidden text-neutral-900 dark:text-[#EFE9E9]">
-      <nav className="flex justify-between ml-3 lg:pt-2">
-        <div className="flex items-center  ">
+    <div className="min-h-screen lg:min-h-fit 2xl:max-w-screen-xl lg:h-fit  lg:mx-auto p-4 bg-[#EFE9E9] dark:bg-neutral-900 lg:overflow-y-hidden text-neutral-900 dark:text-[#EFE9E9]">
+      <nav className="flex justify-between items-center mx-3">
+        <div className="flex items-center">
           <Link href={``} locale="fa" className="mr-2">
             <Image
               src="https://i.postimg.cc/4NgJFCQ9/iran-flag.png"
@@ -58,25 +54,26 @@ function Layout({ children }) {
               height={24}
             />
           </Link>
+        </div>
 
+        <div className="flex justify-between items-center">
           <button
             onClick={handleDarkModeToggle}
-            className="hover:text-yellowPrimary duration-300"
+            className="hover:text-yellowPrimary duration-300 mr-2"
           >
             {darkMode ? <Moon /> : <Sun1 />}
           </button>
-        </div>
 
-        {pathname !== "/" ? (
-          <Link
-            href="/"
-            className="text-4xl right-5 md:right-8 top-3 md:top-5 fixed  z-50"
-          >
-            <CloseCircle className="text-grayText" />
-          </Link>
-        ) : (
-          ""
-        )}
+          <span className="min-w-[24px] min-h-[24px] inline-flex justify-center items-center right-6">
+            {pathname !== "/" ? (
+              <Link href="/" className="text-4xl fixed float-right z-50">
+                <CloseCircle className="text-grayText" />
+              </Link>
+            ) : (
+              ""
+            )}
+          </span>
+        </div>
       </nav>
       <main
         className={`h-full  ${
